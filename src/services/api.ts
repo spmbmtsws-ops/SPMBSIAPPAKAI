7f4fb6f3c6524e18b5f449fd2c2ddd3506882459
spmbmtsws-ops <spmbmtsws@gmail.com>
2026-04-16 12:12:05 +0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = ""; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzc7nNxNvc1CsmmMFJ85kYVzUp_yGQNl1YilH9xT-bM3zrO_IljK6bRbKCuTzbhbsQy/exec"; 
 
 export interface FormField {
   id: string;
